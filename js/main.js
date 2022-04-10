36966d74ac331472a1fc697fd3e34f21f43ac18a
AntonKurtsev <kucrev@gmail.com>
2022-04-10 15:53:09 +0300

    add: create .rewievs

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 $(document).ready(function () {
     
-    
     $('.faq-accordeon__question').click(function () {
         $('.faq-accordeon__question').not($(this)).removeClass('faq-accordeon__question--active');
         $(this).toggleClass('faq-accordeon__question--active');
         $('.faq-accordeon__answer').not($(this).next()).slideUp(500);
         $(this).next().slideToggle(500);
     });
+
+    const swiper = new Swiper('.rewievs-slider', {
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.rewievs-slider__pagination',
+            clickable: true,
+        },
+    });
+
 });
